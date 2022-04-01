574c62cd51dda9475a925e4a6c2887f3de8af98f
Federico Amormino <federico.amormino@gmail.com>
2022-04-01 11:09:58 -0300

    agregando state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,27 @@
+import {useState} from "react";
+
+import memeData from "../memeData";
+
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState("");
+
+  function getMemeImage() {
+    const memesArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input className="form--input" placeholder="Top text" type="text" />
         <input className="form--input" placeholder="Bottom text" type="text" />
-        <button className="form--button" type="button">
+        <button className="form--button" type="button" onClick={getMemeImage}>
           Get a new meme image 🔎
         </button>
-      </form>
+      </div>
+      <img className="form--image" src={memeImage} />
     </main>
   );
 }
